66bad62c6238e9878781bb283f819b9961ee39c2
MacJ13 <jacdim02@gmail.com>
2024-01-02 23:42:15 +0100

    Create image object with image html elements

--- src/helpers/imageElements.ts
+++ src/helpers/imageElements.ts
@@ -1,8 +1,48 @@
 // image elements
-export const createImageElement = (src: string): HTMLImageElement => {
+const createImageElement = (src: string): HTMLImageElement => {
   const image = new Image();
 
   image.src = src;
 
   return image;
+};
+
+const path = "./../../images/snake/";
+
+const food = createImageElement(`${path}apple.png`);
+
+const headUp = createImageElement(`${path}head_up.png`);
+const headDown = createImageElement(`${path}head_down.png`);
+const headRight = createImageElement(`${path}head_right.png`);
+const headLeft = createImageElement(`${path}head_left.png`);
+
+const tailUp = createImageElement(`${path}tail_up.png`);
+const tailDown = createImageElement(`${path}tail_down.png`);
+const tailRight = createImageElement(`${path}tail_right.png`);
+const tailLeft = createImageElement(`${path}tail_left.png`);
+
+const bodyVertical = createImageElement(`${path}body_vertical.png`);
+const bodyHorizontal = createImageElement(`${path}body_horizontal.png`);
+
+const bodyBottomLeft = createImageElement(`${path}body_bottomleft.png`);
+const bodyBottomRight = createImageElement(`${path}body_bottomright.png`);
+const bodyTopLeft = createImageElement(`${path}body_topleft.png`);
+const bodyTopRight = createImageElement(`${path}body_topright.png`);
+
+export const images = {
+  food,
+  headUp,
+  headDown,
+  headRight,
+  headLeft,
+  tailUp,
+  tailDown,
+  tailRight,
+  tailLeft,
+  bodyVertical,
+  bodyHorizontal,
+  bodyBottomLeft,
+  bodyBottomRight,
+  bodyTopLeft,
+  bodyTopRight,
 };
